71d7bade618dbbdcb1d1c517a2ec57201f76e2b9
Kgothatso <kgothatsochauke3@gmail.com>
2026-09-22 12:11:49 +0200

    add unit test for get book service

--- src/test/java/com/springboot/BookApplication/service/BookServiceTest.java
+++ src/test/java/com/springboot/BookApplication/service/BookServiceTest.java
@@ -3,6 +3,7 @@ package com.springboot.BookApplication.service;
 import com.springboot.BookApplication.dto.BookRequestDto;
 import com.springboot.BookApplication.dto.BookResponseDto;
 import com.springboot.BookApplication.entity.Book;
+import com.springboot.BookApplication.exception.BookNotFoundException;
 import com.springboot.BookApplication.mapper.BookMapper;
 import com.springboot.BookApplication.repository.BookRepository;
 import org.junit.jupiter.api.Test;
@@ -11,9 +12,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ExtendWith(MockitoExtension.class)
 public class BookServiceTest {
@@ -49,8 +54,45 @@ public class BookServiceTest {
         verify(bookMapper).toEntity(requestDto);
         verify(bookRepository).save(book);
         verify(bookMapper).toResponseDto(savedBook);
+    }
 
+    @Test
+    void getBookById_shouldReturnBookResponseDto(){
 
+        //Arrange
+        Long id = 1L;
+        Book book = new Book();
+        BookResponseDto responseDto = new BookResponseDto();
+
+        when(bookRepository.findById(id)).thenReturn(Optional.of(book));
+        when(bookMapper.toResponseDto(book)).thenReturn(responseDto);
+
+        //Act
+        BookResponseDto result = bookService.getBookById(id);
+
+        //Assert
+        assertThat(result).isSameAs(responseDto);
+
+        verify(bookRepository).findById(id);
+        verify(bookMapper).toResponseDto(book);
+    }
+
+    @Test
+    void getBookById_shouldThrowExceptionWhenBookDoesNotExist(){
+
+        //Arrange
+        Long id = 999L;
+        Book book = new Book();
+        BookResponseDto responseDto = new BookResponseDto();
+
+        when(bookRepository.findById(id)).thenReturn(Optional.empty());
+
+        //Act and Assert
+        assertThatThrownBy(()-> bookService.getBookById(id))
+                .isInstanceOf(BookNotFoundException.class)
+                .hasMessage("Book with id 999 not found");
+
+        verify(bookRepository).findById(id);
     }
 
 }
